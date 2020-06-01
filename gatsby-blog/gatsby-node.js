dc0c80fd8f7cea177276ae76eb3bab60e6b5104f
曾银林 <757722417@qq.com>
2020-06-02 01:58:30 +0800

    first_day

--- gatsby-blog/gatsby-node.js
+++ gatsby-blog/gatsby-node.js
@@ -1,10 +1,13 @@
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
+// 页面创建函数
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
   const blogPost = path.resolve(`./src/templates/blog-post.js`)
+  //查询md文件构建页面
+  // 此处是用graphql查询
   const result = await graphql(
     `
       {
@@ -31,16 +34,16 @@ exports.createPages = async ({ graphql, actions }) => {
     throw result.errors
   }
 
-  // Create blog posts pages.
+  //  创建博客贴文页面
   const posts = result.data.allMarkdownRemark.edges
-
+  
   posts.forEach((post, index) => {
     const previous = index === posts.length - 1 ? null : posts[index + 1].node
     const next = index === 0 ? null : posts[index - 1].node
-
+    // 生成单个页面函数
     createPage({
-      path: post.node.fields.slug,
-      component: blogPost,
+      path: post.node.fields.slug,//页面路径
+      component: blogPost, //页面使用的模板
       context: {
         slug: post.node.fields.slug,
         previous,
@@ -50,6 +53,7 @@ exports.createPages = async ({ graphql, actions }) => {
   })
 }
 
+// 节点创建函数
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
 
